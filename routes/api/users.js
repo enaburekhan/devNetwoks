fe4873af8b733735e3fd7b3e82e3538619847f92
enaburekhan <ericenaburekhan2018@gmail.com>
2021-09-23 19:28:22 +0100

    Implement jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator');
 const bycrpt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -53,7 +55,21 @@ router.post('/', [
 
       // Return jsonwebtoken
 
-    res.send('User registered') 
+     const payload =  {
+       user: {
+         id: user.id  
+       }
+     }
+
+     jwt.sign(
+       payload,
+       config.get('jwtSecret'),
+       { expiresIn: 360000 },
+       (err, token) => {
+         if (err) throw err;
+         res.json({ token })
+       }
+       );
 
     } catch(err){
       console.log(err.message);
