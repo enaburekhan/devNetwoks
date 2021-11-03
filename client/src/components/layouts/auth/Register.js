f3c9122d4569420dd69fcffec78ec1eed92eee8a
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-03 22:54:58 +0100

    Create login form and request example

--- client/src/components/layouts/auth/Register.js
+++ client/src/components/layouts/auth/Register.js
@@ -1,4 +1,5 @@
 import { Fragment, useState } from "react";
+import { Link } from 'react-router-dom';
 
 const Register = () => {
 
@@ -13,12 +14,32 @@ const Register = () => {
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if(password !== password2){
         console.log('password do not match');
     }else{
-        console.log(formData);
+        console.log('success');
+        // const newUser = {
+        //   name,
+        //   email,
+        //   password    
+        // };
+
+        // try {
+        //    const config = {
+        //      headers: {
+        //        'Content-type': 'application/json'    
+        //      }    
+        //    } 
+        //    const body = JSON.stringify(newUser);
+
+        //    const res = await axios.post('/api/users', body, config);
+        //    console.log(res.data);
+           
+        // } catch (err) {
+        //   console.error(err.response.data);  
+        // }
     }    
   }
   return ( 
@@ -75,7 +96,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>    
      </Fragment> 
    );
