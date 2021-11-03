785f5b1da63cf5bdad106fd7e748cdc348bdedb1
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-03 15:58:11 +0100

    React-router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';  
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
 import './App.css';
+import Register from './components/layouts/auth/Register';
+import Login from './components/layouts/auth/Login';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
     </Fragment>
+    </Router>
   );
 }
 
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <nav className="navbar bg-dark">
         <h1>
-          <a href="index.html"><i className="fas fa-code"></i> DevNetworks</a>
+          <Link to="/"><i className="fas fa-code"></i> DevNetworks</Link>
         </h1>
         <ul>
-          <li><a href="profiles.html">Developers</a></li>
-          <li><a href="register.html">Register</a></li>
-          <li><a href="login.html">Login</a></li>
+          <li><a href="!#">Developers</a></li>
+          <li><Link to="/register">Register</Link></li>
+          <li><Link to="/login">Login</Link></li>
         </ul>
       </nav>    
      );
